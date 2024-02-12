e1240016df57a23961f7661d349ee54136dbd878
MichaelGee76 <gottungmichael@gmail.com>
2024-02-12 15:26:16 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,140 @@
+const rpsArray = ["rock", "paper", "scissors"];
+
+//  ################### Get Players Choice #################
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+//  ################### Output #########################
+
+const winner = document.querySelector(".outputMiddle");
+const playerOutput = document.querySelector(".outputLeft");
+const computerOutput = document.querySelector(".outputRight");
+const count = document.querySelector(".count");
+const playerCount = document.querySelector(".playerCount");
+const computerCount = document.querySelector(".computerCount");
+const heading = document.querySelector("h1");
+
+// #################### Function Reset, RandomNumber, ComputerChoice #######################
+
+function reset() {
+    countPlayer = 0;
+    countComputer = 0;
+    playerOutput.textContent = "";
+    computerOutput.textContent = "";
+    winner.textContent = "";
+    count.textContent = "";
+    heading.style.color = "#f4eee0";
+    computerCount.textContent = "";
+    playerCount.textContent = "";
+    countPlayer = 0;
+    countComputer = 0;
+    round = 0;
+}
+
+function randomNumber() {
+    let ranNum = Math.floor(Math.random() * 3);
+    return ranNum;
+}
+
+function getComputerChoice() {
+    const computerChoice = rpsArray[randomNumber()];
+    return computerChoice.toUpperCase();
+}
+
+// ##################### eventListeners ######################
+
+rock.addEventListener("click", () => {
+    rps("rock");
+});
+
+paper.addEventListener("click", () => {
+    rps("paper");
+});
+
+scissors.addEventListener("click", () => {
+    rps("scissors");
+});
+
+// ################### Game Function #########################
+
+let countPlayer = 0;
+let countComputer = 0;
+let round = 0;
+
+function rps(choice) {
+    let computerChoice = getComputerChoice();
+    let playerChoice = choice.toUpperCase();
+
+    if (playerChoice === "ROCK" && computerChoice === "SCISSORS") {
+        countPlayer++;
+        console.log(countPlayer);
+
+        round++;
+        winner.textContent = "You win. Rock beats Scissors.";
+        playerCount.textContent = `Your Score ${countPlayer}`;
+        playerOutput.textContent = playerChoice;
+        computerOutput.textContent = computerChoice;
+        computerCount.textContent = `Computer Score ${countComputer}`;
+        count.textContent = `Round ${round}`;
+    } else if (playerChoice === "SCISSORS" && computerChoice === "PAPER") {
+        countPlayer++;
+        console.log(countPlayer);
+
+        round++;
+        winner.textContent = "You win. Scissors beats Paper.";
+        playerOutput.textContent = playerChoice;
+        playerCount.textContent = `Your Score ${countPlayer}`;
+        computerOutput.textContent = computerChoice;
+        computerCount.textContent = `Computer Score ${countComputer}`;
+        count.textContent = `Round ${round}`;
+    } else if (playerChoice === "PAPER" && computerChoice === "ROCK") {
+        countPlayer++;
+        console.log(countPlayer);
+
+        round++;
+        winner.textContent = "You win. Paper beats Rock.";
+        playerOutput.textContent = playerChoice;
+        playerCount.textContent = `Your Score ${countPlayer}`;
+        computerCount.textContent = `Computer Score ${countComputer}`;
+        computerOutput.textContent = computerChoice;
+        count.textContent = `Round ${round}`;
+    } else if (playerChoice === computerChoice) {
+        // round++;
+        // playerOutput.textContent = playerChoice;
+        // playerCount.textContent = `Your Score ${countPlayer}`;
+        // computerOutput.textContent = computerChoice;
+        // computerCount.textContent = `Computer Score ${countComputer}`;
+        // winner.textContent = "It's a tie!";
+        // count.textContent = `Round ${round}`;
+    } else {
+        countComputer++;
+        console.log("computer", countComputer);
+
+        round++;
+        playerOutput.textContent = playerChoice;
+        computerOutput.textContent = computerChoice;
+        computerCount.textContent = `Computer Score ${countComputer}`;
+        winner.textContent = `You loose! ${computerChoice} beats  ${playerChoice} !`;
+        count.textContent = `Round ${round}`;
+    }
+    // Fragt den Gewinner ab
+    if (countPlayer >= 5) {
+        heading.style.color = "red";
+        heading.textContent = " You win the game";
+        setTimeout(function () {
+            heading.textContent = "Rock Paper Scissors";
+            reset();
+        }, 5000);
+        return;
+    } else if (countComputer >= 5) {
+        heading.style.color = "red";
+        heading.textContent = " Computer wins the game";
+        setTimeout(function () {
+            heading.textContent = "Rock Paper Scissors";
+            reset();
+        }, 5000);
+        return;
+    }
+}
